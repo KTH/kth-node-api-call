9a102494d32f36a03aa6d78f82fcb92bf99fb022
Jonas Lantto <jonas.e.lantto@gmail.com>
2017-03-28 11:11:59 +0200

    testing api key check functionality

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 
 const expect = require('chai').expect
+const sinon = require('sinon')
 
 const nock = require('nock')
 
@@ -14,48 +15,15 @@ const mockAPI = nock('http://localhost:3001').get('/api/test/_paths').reply(200,
       type: 'api_key'
     }
   }
-})
-
-mockAPI.get('/api/test/_checkAPIKey').reply(401, {})
-
-// const mockery = require('mockery')
-
-// const mockApiClient = function () {}
-
-// mockApiClient.prototype.getAsync = function () {
-//   return Promise.resolve({
-//     statusCode: 200,
-//     body: {
-//       api: {
-//         'path1': {
-//           'uri': '/api/test/v1/path1/:param1',
-//           'method': 'PUT',
-//           'apikey': {
-//             'scope_required': true,
-//             'scopes': ['write'],
-//             'type': 'api_key'
-//           }
-//         },
-//         'path2': {
-//           'uri': '/api/test/v1/test/path2/:param',
-//           'method': 'GET',
-//           'apikey': {
-//             'scope_required': true,
-//             'scopes': ['read'],
-//             'type': 'api_key'
-//           }
-//         }
-//       }
-//     }
-//   })
-// }
+}).get('/api/test/_checkAPIkey').reply(401, {})
 
 const mockApiConfig = {
   testApi: {
     https: false,
     port: 3001,
     host: 'localhost',
-    proxyBasePath: '/api/test'
+    proxyBasePath: '/api/test',
+    required: true
   }
 }
 
@@ -64,6 +32,7 @@ const mockApiKeyConfig = {
 }
 const mockLogger = {}
 mockLogger.debug = mockLogger.error = mockLogger.warn = mockLogger.info = () => {}
+// mockLogger.debug = mockLogger.error = mockLogger.warn = mockLogger.info = console.log
 
 const opts = {
   log: mockLogger,
@@ -73,17 +42,28 @@ const opts = {
   checkAPIs: true // performs api-key checks against the apis, if a 'required' check fails, the app will exit. Required apis are specified in the config
 }
 
-// mockery.enable({
-//   warnOnReplace: false,
-//   warnOnUnregistered: false
-// })
-
-// mockery.registerMock('./basic', mockApiClient)
-
 const connections = require('../connections')
 
 describe('Testing api', function () {
   it('should set up connections', function (done) {
+    this.originalProcess = process.exit
+    Object.defineProperty(process, 'exit', { // mocking out process globally
+      value: sinon.spy()
+    })
+    const output = connections.setup(mockApiConfig, mockApiKeyConfig, opts)
+    setTimeout(function () {
+      expect(output.testApi.connected).to.be.true
+      expect(process.exit.called).to.be.true
+      done()
+    }, 500) // wait for setup to finish
+  })
+  it('should set up connections', function (done) {
+    Object.defineProperty(process, 'exit', { // undo previous exit override
+      value: this.originalProcess
+    })
+    mockAPI
+    .get('/api/test/_paths').reply(200, {})
+    .get('/api/test/_checkAPIkey').reply(200, {})
     const output = connections.setup(mockApiConfig, mockApiKeyConfig, opts)
     setTimeout(function () {
       expect(output.testApi.connected).to.be.true
