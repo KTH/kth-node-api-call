3b7a335b0443bb3007059fef8012aaacb8b6be4b
Rickard Falk <rickard.falk@gmail.com>
2021-12-03 09:02:21 +0100

    test: create api test file for jest

---
+++ api.test.js
@@ -0,0 +1,14 @@
+describe('Testing api', () => {
+  it('should shut down on bad API key', async () => {
+    // this.originalProcess = process.exit
+    // Object.defineProperty(process, 'exit', {
+    //   // mocking out process globally
+    //   value: sinon.spy(),
+    // })
+    // connections.setup(mockApiConfig, mockApiKeyConfig, opts)
+    // setTimeout(function () {
+    //   // eslint-disable-next-line no-unused-expressions
+    //   expect(process.exit.called).to.be.true
+    // }, 500) // wait for setup to finish
+  })
+})
