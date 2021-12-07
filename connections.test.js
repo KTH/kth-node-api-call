89d0e1da44e49373b2d9f8fa8b8cc1ee8decba80
Rickard Falk <rickard.falk@gmail.com>
2021-12-07 16:07:35 +0100

    test: should set up connections

--- connections.test.js
+++ connections.test.js
@@ -37,17 +37,14 @@ const opts = {
   checkAPIs: true, // performs api-key checks against the apis, if a 'required' check fails, the app will exit. Required apis are specified in the config
 }
 
+const paths = {}
+
 jest.mock('./basic', () =>
   jest.fn().mockImplementation(() => ({
-    getAsync: ({ uri }) => {
-      const paths = {
-        '/api/test/_paths': { statusCode: 200, body: {} },
-        '/api/test/_checkAPIkey': { statusCode: 401, body: {} },
-      }
-      return new Promise((resolve, reject) =>
+    getAsync: uri =>
+      new Promise((resolve, reject) =>
         process.nextTick(() => (paths[uri] ? resolve(paths[uri]) : reject(new Error('Path ' + uri + ' not found.'))))
-      )
-    },
+      ),
   }))
 )
 
@@ -55,6 +52,9 @@ describe('Testing connection', () => {
   it(IS_ACCESSIBLE, () => expect(connections.setup).toBeFunction())
 
   it('should shut down on bad API key', done => {
+    paths['/api/test/_paths'] = { statusCode: 200, body: {} }
+    paths['/api/test/_checkAPIkey'] = { statusCode: 401, body: {} }
+
     const originalExit = process.exit
     const mockedExit = jest.fn()
     global.process.exit = mockedExit
@@ -63,6 +63,33 @@ describe('Testing connection', () => {
     setTimeout(() => {
       global.process.exit = originalExit
       expect(mockedExit).toBeCalledWith(1)
+      done()
+    }, 500) // wait for setup to finish
+  })
+
+  it('should set up connections', done => {
+    paths['/api/test/_paths'] = {
+      statusCode: 200,
+      body: {
+        path1: {
+          uri: '/api/test/v1/path1/:param1',
+          method: 'GET',
+          apikey: {
+            scope_required: true,
+            scopes: ['read'],
+            type: 'api_key',
+          },
+        },
+      },
+    }
+    paths['/api/test/_checkAPIkey'] = { statusCode: 200, body: {} }
+    const originalExit = process.exit
+    const mockedExit = jest.fn()
+    global.process.exit = mockedExit
+    const output = connections.setup(mockApiConfig, mockApiKeyConfig, opts)
+    setTimeout(() => {
+      global.process.exit = originalExit
+      expect(output.testApi.connected).toBeTrue()
       done()
     }, 500) // wait for setup to finish
   })
