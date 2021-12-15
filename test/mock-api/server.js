481965e17cded8384f1f0c5d6c074842f55e13f4
Mikael Bengtsson <mibengt@kth.se>
2021-12-15 10:51:54 +0100

    Mock-api server

---
+++ test/mock-api/server.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+
+'use strict'
+
+const express = require('express')
+const config = require('./config')
+
+const app = express()
+config.paths.forEach(path => {
+  // console.log('Added path', path.url)
+  app[path.method](path.url, (req, res) => {
+    // console.log('Responded on path', path.url)
+    if (path.response) {
+      res.status(path.response.statusCode).send(path.response.body)
+    } else res.destroy(null)
+  })
+})
+
+app.use((req, res) => {
+  // console.log('Caught request on path', req.url)
+  res.status(404).send('')
+})
+process.on('SIGTERM', () => {
+  process.exit(0)
+})
+
+app.listen(config.host.port, config.host.address)
