338b64afd3a9a17cad46bca02e801c0e1d3acd71
Mikael Bengtsson <mibengt@kth.se>
2021-12-20 16:28:11 +0100

    test: timeout test + cleanup

--- basic.test.js
+++ basic.test.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-const { IS_ACCESSIBLE } = require('./test-utils')
 
 const BasicAPI = require('./basic')
 const apiserver = require('./test/mock-api/server')
@@ -18,11 +17,12 @@ const opts = {
   port: '3210',
   https: false,
   json: true,
-  defaultTimeout: 2790,
+  defaultTimeout: 50,
   headers: { 'Content-Type': 'application/json' },
   retryOnESOCKETTIMEDOUT: true,
-  maxNumberOfRetries: 7,
+  maxNumberOfRetries: 2,
   basePath: '/api/test',
+  log: mockLogger,
 }
 
 const api = BasicAPI(opts)
@@ -101,302 +101,19 @@ describe('basic calls works as expected', () => {
     expect(result.body).toBeUndefined()
     expect(result.statusCode).toBe(200)
   })
-  afterAll(async () => {
-    await api.getAsync('/goodbye')
+  xit('should retry on timeout', async () => {
+    api._request.post = jest.fn().mockImplementation(async (options, callback) => {
+      callback(new Error('ESOCKETTIMEDOUT'))
+    })
+
+    await api.postAsync('/timeout').catch(e => {
+      expect(e.message).toContain('timed out after 2 retries. The connection to the API seems to be overloaded.')
+      expect(api._request.post).toBeCalledTimes(3)
+    })
+  })
+
+  afterAll(done => {
+    api.getAsync('/goodbye')
+    setTimeout(done, 500)
   })
 })
-/*
-const { EXPECTS, RETURNS, IS_ACCESSIBLE, bold, copyObject, WORKS } = require('./test-utils')
-
-function runTestsAboutPrototypeMethods() {
-  describe(`has a prototype which`, () => {
-    const { prototype } = BasicApiExport
-
-    const expectedMethods = [
-      { name: 'get', caption: 'two arguments (options, callback)', length: 2 },
-      { name: 'getAsync', caption: 'one argument (options)', length: 1 },
-      { name: 'post', caption: 'two arguments (options, callback)', length: 2 },
-      { name: 'postAsync', caption: 'one argument (options)', length: 1 },
-      { name: 'put', caption: 'two arguments (options, callback)', length: 2 },
-      { name: 'putAsync', caption: 'one argument (options)', length: 1 },
-      { name: 'del', caption: 'two arguments (options, callback)', length: 2 },
-      { name: 'delAsync', caption: 'one argument (options)', length: 1 },
-      { name: 'head', caption: 'two arguments (options, callback)', length: 2 },
-      { name: 'headAsync', caption: 'one argument (options)', length: 1 },
-      { name: 'patch', caption: 'two arguments (options, callback)', length: 2 },
-      { name: 'patchAsync', caption: 'one argument (options)', length: 1 },
-      { name: 'cookie', caption: 'one argument (cookie)', length: 1 },
-      { name: 'jar', caption: 'no arguments', length: 0 },
-      { name: 'defaults', caption: 'two arguments (options)', length: 1 },
-      { name: 'resolve', caption: 'two arguments (uri, params)', length: 2 },
-    ]
-
-    const maxNameLength = Math.max(...expectedMethods.map(item => item.name.length))
-
-    it(`is named "BasicAPI"`, () => expect(prototype.constructor.name).toBe('BasicAPI'))
-
-    it(`contains the expected ${expectedMethods.length} methods`, () => {
-      expect(prototype).toContainAllKeys(expectedMethods.map(item => item.name))
-    })
-
-    expectedMethods.forEach(({ name, caption, length }) => {
-      const method = bold(`${name}()`.padEnd(maxNameLength + 2, ' '))
-      it(`has a method ${method} that ${IS_ACCESSIBLE} and ${EXPECTS} ${caption}`, () => {
-        expect(prototype[name]).toBeFunction()
-        expect(prototype[name]).toHaveLength(length)
-      })
-    })
-  })
-}
-
-function getTestOptions(target) {
-  switch (target) {
-    case 'full':
-      return {
-        protocol: 'http',
-        https: false,
-        host: 'localhost',
-        hostname: '127.0.0.1',
-        port: '3007',
-        headers: { 'Content-Type': 'application/json' },
-        json: { dummy: 'test' },
-        redis: DummyRedisMockup,
-        basePath: '/api/program',
-        defaultTimeout: 2790,
-        retryOnESOCKETTIMEDOUT: true,
-        maxNumberOfRetries: 7,
-        log: DummyLogMockup,
-      }
-
-    default:
-      throw new Error('Invalid target')
-  }
-}
-
-describe.skip('Exported function "BasicAPI"', () => {
-  it(IS_ACCESSIBLE, () => expect(BasicApiExport).toBeFunction())
-
-  runTestsAboutPrototypeMethods()
-
-  it(`${EXPECTS} two arguments (options, base)`, () => expect(BasicApiExport).toHaveLength(2))
-
-  it(`- when used w/o arguments - ${RETURNS} an instance of class "BasicAPI"`, () => {
-    const result = BasicApiExport()
-
-    expect(result).toBeObject()
-    expect(result.constructor.name).toBe('BasicAPI')
-
-    const copy = copyObject(result, { replaceFunctions: true })
-    expect(copy).toEqual({
-      _basePath: '',
-      _defaultTimeout: 2000,
-      _hasRedis: false,
-      _log: undefined,
-      _maxNumberOfRetries: 5,
-      _redis: undefined,
-      _request: {
-        _self: '(MOCK:0 calls)',
-        props: {
-          defaults: '(MOCK:1 call)',
-          get: '(MOCK:0 calls)',
-        },
-      },
-      _retryOnESOCKETTIMEDOUT: undefined,
-    })
-  })
-
-  it(`- when used with empty "options" - ${RETURNS} an instance of class "BasicAPI"`, () => {
-    const result = BasicApiExport({})
-
-    expect(result).toBeObject()
-    expect(result.constructor.name).toBe('BasicAPI')
-
-    const copy = copyObject(result, { replaceFunctions: true })
-    expect(copy).toEqual({
-      _basePath: '',
-      _defaultTimeout: 2000,
-      _hasRedis: false,
-      _log: undefined,
-      _maxNumberOfRetries: 5,
-      _redis: undefined,
-      _request: {
-        _self: '(MOCK:0 calls)',
-        props: {
-          defaults: '(MOCK:1 call)',
-          get: '(MOCK:0 calls)',
-        },
-      },
-      _retryOnESOCKETTIMEDOUT: undefined,
-    })
-  })
-
-  it(`- when used with all valid "options" - ${RETURNS} an instance of class "BasicAPI"`, () => {
-    const options = getTestOptions('full')
-
-    const result = BasicApiExport(options)
-
-    expect(result).toBeObject()
-    expect(result.constructor.name).toBe('BasicAPI')
-
-    const copy = copyObject(result, { replaceFunctions: true })
-    expect(copy).toEqual({
-      _basePath: '/api/program',
-      _defaultTimeout: 2790,
-      _hasRedis: true,
-      _log: {
-        log: '(MOCK:0 calls)',
-      },
-      _maxNumberOfRetries: 7,
-      _redis: {
-        client: {},
-      },
-      _request: {
-        _self: '(MOCK:0 calls)',
-        props: {
-          defaults: '(MOCK:1 call)',
-          get: '(MOCK:0 calls)',
-        },
-      },
-      _retryOnESOCKETTIMEDOUT: true,
-    })
-  })
-
-  it(`- when used with all valid "options" - ${WORKS} as expected`, () => {
-    const options = getTestOptions('full')
-
-    const api = BasicApiExport(options)
-
-    const allCalls = copyObject(AllMockups, { replaceFunctions: true })
-    expect(allCalls).toEqual({
-      DummyLogMockup: {
-        log: '(MOCK:0 calls)',
-      },
-      DummyRedisMockup: {
-        client: {},
-      },
-      RequestMockup: {
-        _self: '(MOCK:0 calls)',
-        props: {
-          defaults: '(MOCK:1 call)',
-          get: '(MOCK:0 calls)',
-        },
-      },
-    })
-
-    expect(RequestMockup.defaults).toHaveBeenCalledWith({
-      baseUrl: 'http://localhost:3007',
-      headers: options.headers,
-      json: options.json,
-      pool: { maxSockets: Infinity },
-    })
-
-    expect(api._request).toBe(RequestMockup)
-  })
-
-  it(`- when used ${bold('twice')} with same valid "options" - ${RETURNS} two different objects`, () => {
-    const options = getTestOptions('full')
-
-    const result1 = BasicApiExport(options)
-    const result2 = BasicApiExport(options)
-
-    expect(result1).not.toBe(result2)
-  })
-
-  describe(`returns an ${bold('instance of class "BasicAPI"')} that`, () => {
-    const options = getTestOptions('full')
-
-    let api
-    beforeAll(() => {
-      api = BasicApiExport(options)
-    })
-
-    it(`has a method get(options, callback) with expected behaviour`, async () => {
-      const get = api.get.bind(api)
-
-      expect(get).toThrow('Cannot read propert') // Originally 'property', changed to accommodate 'properties' as well
-
-      get('test/1')
-
-      const allCalls = copyObject(AllMockups, { replaceFunctions: true })
-      expect(allCalls).toEqual({
-        DummyLogMockup: {
-          log: '(MOCK:0 calls)',
-        },
-        DummyRedisMockup: {
-          client: {},
-        },
-        RequestMockup: {
-          _self: '(MOCK:0 calls)',
-          props: {
-            defaults: '(MOCK:0 calls)',
-            get: '(MOCK:1 call)',
-          },
-        },
-      })
-
-      expect(RequestMockup.get).toHaveBeenCalledTimes(1)
-      const getCallArguments = copyObject(RequestMockup.get.mock.calls[0], { replaceFunctions: true })
-      expect(getCallArguments).toEqual([
-        {
-          headers: { 'request-guid': getCallArguments[0].requestGuid },
-          requestGuid: getCallArguments[0].requestGuid,
-          uri: '/api/programtest/1',
-        },
-        '(FUNC:anonymous)',
-      ])
-    })
-  })
-})
-*/
-/*
-
-  xit('should retry GET,POST,PUT,DELETE,PATCH,HEAD call on ESOCKETTIMEDOUT', done => {
-    paths['/api/test/_paths'] = [
-      {
-        statusCode: 200,
-        body: {
-          path1: {
-            uri: '/api/test/v1/path1/:param1',
-            method: 'GET',
-            apikey: {
-              scope_required: true,
-              scopes: ['read'],
-              type: 'api_key',
-            },
-          },
-        },
-      },
-    ]
-    paths['/api/test/_checkAPIkey'] = [{ statusCode: 200, body: {} }]
-    paths['/api/test/error'] = Array(6 * 6).fill({ answerTimeout: true })
-
-    const output = connections.setup(mockApiConfig, mockApiKeyConfig, { ...opts, retryOnESOCKETTIMEDOUT: true })
-    setTimeout(() => {
-      const { client } = output.testApi
-      client.getAsync('/api/test/error').catch(e1 => {
-        console.log(paths['/api/test/error'].length)
-        expect(e1.message).toContain('retries. The connection to the API seems to be overloaded.')
-        done()
-      })
-    }, 500) // wait for setup to finish
-  })
-
-
-*/
-
-/*
-const BasicAPI = require('./basic')
-    const opts = {
-      hostname: "www.google.com",
-      port: 443,
-      https: true,
-      json: true,
-      defaultTimeout: 600,
-      headers: {},
-      retryOnESOCKETTIMEDOUT: false,
-      maxNumberOfRetries: 2,
-      log:  {},
-    }
-  const c=new BasicAPI(opts)
-   c.getAsync("/") 
-*/
