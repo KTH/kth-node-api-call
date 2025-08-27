7b63c6d8cb53c2c19787d71a4411ce862aac8819
Niklas Sandström <mr.n.sandstrom@gmail.com>
2025-08-27 10:52:52 +0200

    simplify index test

--- index.test.js
+++ index.test.js
@@ -1,17 +1,9 @@
 const apiCall = require('./index')
 
-const { copyObject } = require('./test-utils')
-
 describe('Package "@kth/api-call"', () => {
   it(`has all the expected exports`, () => {
-    const copy = copyObject(apiCall, { replaceFunctions: true })
-    expect(copy).toMatchInlineSnapshot(`
-      {
-        "BasicAPI": "(FUNC:BasicAPI)",
-        "Connections": {
-          "setup": "(FUNC:setup)",
-        },
-      }
-    `)
+    expect(typeof apiCall.BasicAPI).toBe('function')
+    expect(typeof apiCall.Connections).toBe('object')
+    expect(typeof apiCall.Connections.setup).toBe('function')
   })
 })
